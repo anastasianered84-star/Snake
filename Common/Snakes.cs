4947bfd772ef5ec439b0da5fd371934ea1df3c68
Lobanov Mikhail <axxuetam@gmail.com>
2025-11-13 15:00:28 +0500

    реализован класс Snakes.Фефелова

---
+++ Common/Snakes.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class Snakes
+    {
+        public class Point
+        {
+            public int X;
+            public int Y;
+
+            public Point(int X, int Y)
+            {
+                this.X = X;
+                this.Y = Y;
+            }
+
+            public Point() { }
+        }
+
+        public enum Direction
+        {
+            Left,
+            Right,
+            Up,
+            Down,
+            Start
+        }
+
+        public List<Point> Points = new List<Point>();
+        public Direction direction = Direction.Start;
+        public bool GameOver = false;
+
+    }
+}
