787727e589e3c89b4a15d0e9356f5d129e1c8676
Anastasia <filippov31012007@gmail.com>
2025-11-19 16:46:45 +0500

    Реализован метод Main. Фефелова

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -305,6 +305,20 @@ namespace Snake
 
         static void Main(string[] args)
         {
+            try
+            {
+                Thread tRec = new Thread(new ThreadStart(Receiver));
+                tRec.Start();
+
+                Thread tTime = new Thread(Timer);
+                tTime.Start();
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Возникло исключение: " + ex.ToString() + "\n  " + ex.Message);
+            }
+
         }
     }
 }
